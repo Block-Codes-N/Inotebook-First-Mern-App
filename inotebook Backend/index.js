fb8debf59e7c6a09c69354aded57a2bc77216242
Noman_Shah <nomankhalidofficial@gmail.com>
2023-09-12 20:04:21 +0500

    code issue still exist

--- inotebook Backend/index.js
+++ inotebook Backend/index.js
@@ -1,58 +1,58 @@
 
 
-// const connectToMongo =  require('./db');                          //First code 
-// const express = require('express')
+const connectToMongo =  require('./db');                          //First code 
+const express = require('express')
 
-// connectToMongo();
-// const app = express()
-// const port = 3000
-// app.use(express.json())
-
-// // Available Routes
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-
-
-
-const express = require('express');
-const { default: mongoose } = require('mongoose');
+connectToMongo();
 const app = express()
-const port = 200;
+const port = 3000
 app.use(express.json())
 
-const middleware = (req, res, next) =>{
-  console.log('middleware function called');
-  next();
-}
-
-
-app.get('/', (req, res)=>{
-
-  res.send('Request From main page');
-
-});
-
-app.get('/contact', middleware, (req, res)=>{
-
-  res.send('Request From Contact page');
-
-});
-
-app.get('/about', (req, res)=>{
-
-  res.send('Request From About page');
-
-});
-
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+// const express = require('express');
+// const { default: mongoose } = require('mongoose');
+// const app = express()
+// const port = 200;
+// app.use(express.json())
+
+// const middleware = (req, res, next) =>{
+//   console.log('middleware function called');
+//   next();
+// }
+
+
+// app.get('/', (req, res)=>{
+
+//   res.send('Request From main page');
+
+// });
+
+// app.get('/contact', middleware, (req, res)=>{
+
+//   res.send('Request From Contact page');
+
+// });
+
+// app.get('/about', (req, res)=>{
+
+//   res.send('Request From About page');
+
+// });
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// })
 
 
 
@@ -67,31 +67,27 @@ app.listen(port, () => {
 
 
 
-Router.post('/', async(req, res) =>{
+// Router.post('/', async(req, res) =>{
 
-  const(name,password,email) = req.body;
-  if(!name || !email || !password){
-    return res.json({error:'please fill all'});
-  }
+//   const(name,password,email) = req.body;
+//   if(!name || !email || !password){
+//     return res.json({error:'please fill all'});
+//   }
 
-  try {
-    const userExists = await User.findOne({email:email})
+//   try {
+//     const userExists = await User.findOne({email:email})
 
-    if(userExists){
-      return res.json({error: "please use different email"});
-    }
-    else{
-      const user = new User({name,password,email});
-      await user.save();
-      res.json({message: "USer Registered"})
-    }
-  } 
+//     if(userExists){
+//       return res.json({error: "please use different email"});
+//     }
+//     else{
+//       const user = new User({name,password,email});
+//       await user.save();
+//       res.json({message: "USer Registered"})
+//     }
+//   } 
   
-  catch (error) {
-    console.log(error);
-  }
-  // catch (error) {
-  //   console.log(error);
-  // }
-
-})
+//   catch (error) {
+//     console.log(error);
+//   }
+// })
--- inotebook Backend/routes/auth.js
+++ inotebook Backend/routes/auth.js
@@ -6,22 +6,81 @@ const { body, validationResult } = require('express-validator');
 
 // Create a User using POST: (/api/auth/) No authentication required.
 
-router.post('/', [
-    body("name").isLength({ min: 3 }),
-    body('email').isEmail().bail().custom(async value => {
-      const existingUser = await User.findByEmail(value);
-      if (existingUser) {
-        // Will use the below as the error message
-        throw new Error('A user already exists with this e-mail address');
-      }
-    }),
-    body("password").isLength({ min: 8 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })  
+// router.post('/', [
+//     body("name").isLength({ min: 3 }),
+//     body('email').isEmail()
+//     // .findUserByEmail = (email) => {
+//     //     // return this.findOne({ email: email })
+//     //   return User.findOne({ email: email })
+//     //   }
+//       // if (existingUser) {
+//       //   // Will use the below as the error message
+//       //   throw new Error('A user already exists with this e-mail address');
+//       // }
+//     ,
+//     body("password").isLength({ min: 8 }),
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req)
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() })  
+//     }
+//       try {
+//     const userExists = await User.findOne({email:email})
+
+//     if(userExists){
+//       return res.json({error: "please use different email"});
+//     }
+//   }
+//   catch (error) {
+//         console.log(error);
+//       }
+
+
+    // User.findUserByEmail = (email) => {
+    //   return this.findOne({ email: email })
+    //   // or return User.findOne({ email: email })
+    // }
+
+  //   User.create({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   }).then(user => res.json(user));
+
+  // });
+    
+    
+    // res.send(req.body);  //Without User.create method.
+    router.post('/', async(req, res) =>{
+
+  // const(name,password,email) = req.body;
+
+  if(!name || !email || !password){
+    return res.json({error:'please fill all'});
+  }
+
+  try {
+    const userExists = await User.findOne({email:email})
+
+    if(userExists){
+      return res.json({error: "please use different email"});
     }
+    else{
+      const user = new User({name,password,email});
+      await user.save();
+      res.json({message: "USer Registered"})
+    }
+  } 
+  
+  catch (error) {
+    console.log(error);
+  }
+})
+
+module.exports = router;
+
+
 
                                                                                                 //   try {
                                                                                                 //     const user = await User.create({
@@ -39,16 +98,3 @@ router.post('/', [
                                                                                                 //     res.status(500).json({ error: 'Server error' });
                                                                                                 //   }
                                                                                                 // });
-
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    }).then(user => res.json(user));
-    
-    
-    // res.send(req.body);  //Without User.create method.
-})
-    
-
-module.exports = router;
