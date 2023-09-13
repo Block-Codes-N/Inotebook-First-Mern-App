e44c7700c9dedc3d80a9e35519409a7dd9e7275b
Noman_Shah <nomankhalidofficial@gmail.com>
2023-09-13 14:03:50 +0500

    Duplicate email error finaly resolved.

--- inotebook Backend/routes/auth.js
+++ inotebook Backend/routes/auth.js
@@ -4,97 +4,42 @@ const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
 
 
-// Create a User using POST: (/api/auth/) No authentication required.
+// Create a User using POST: (/api/auth/createuser) No authentication required.
 
-// router.post('/', [
-//     body("name").isLength({ min: 3 }),
-//     body('email').isEmail()
-//     // .findUserByEmail = (email) => {
-//     //     // return this.findOne({ email: email })
-//     //   return User.findOne({ email: email })
-//     //   }
-//       // if (existingUser) {
-//       //   // Will use the below as the error message
-//       //   throw new Error('A user already exists with this e-mail address');
-//       // }
-//     ,
-//     body("password").isLength({ min: 8 }),
-//   ],
-//   async (req, res) => {
-//     const errors = validationResult(req)
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() })  
-//     }
-//       try {
-//     const userExists = await User.findOne({email:email})
+router.post('/createuser', [
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail().withMessage("Email is required")
+    ,body("password").isLength({ min: 8 }),
+  ],
 
-//     if(userExists){
-//       return res.json({error: "please use different email"});
-//     }
-//   }
-//   catch (error) {
-//         console.log(error);
-//       }
+  async (req, res) => {
+  const errors = validationResult(req)
 
-
-    // User.findUserByEmail = (email) => {
-    //   return this.findOne({ email: email })
-    //   // or return User.findOne({ email: email })
-    // }
-
-  //   User.create({
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: req.body.password
-  //   }).then(user => res.json(user));
-
-  // });
-    
-    
-    // res.send(req.body);  //Without User.create method.
-    router.post('/', async(req, res) =>{
-
-  // const(name,password,email) = req.body;
-
-  if(!name || !email || !password){
-    return res.json({error:'please fill all'});
-  }
-
-  try {
-    const userExists = await User.findOne({email:email})
-
-    if(userExists){
-      return res.json({error: "please use different email"});
+    // if there are errors return bad request
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })  
     }
-    else{
-      const user = new User({name,password,email});
-      await user.save();
-      res.json({message: "USer Registered"})
+
+    //Check wether the user with this email exists already
+    try {
+
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+      return res.status(400).json({error:"Sorry a user with this email already exists"})
     }
+
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }) 
+    res.json(user);
+
   } 
-  
   catch (error) {
-    console.log(error);
+      console.error(error.message);
+      res.status(500).send("Some Error Occured")
   }
-})
+  });
 
 module.exports = router;
-
-
-
-                                                                                                //   try {
-                                                                                                //     const user = await User.create({
-                                                                                                //       name: req.body.name,
-                                                                                                //       email: req.body.email,
-                                                                                                //       password: req.body.password
-                                                                                                //     });
-                                                                                                //     res.json(user);
-                                                                                                //   } catch (error) {
-                                                                                                //     if (error.code === 11000) {
-                                                                                                //       // Duplicate key error
-                                                                                                //       return res.status(400).json({ error: 'Email already exists' });
-                                                                                                //     }
-                                                                                                //     console.error(error);
-                                                                                                //     res.status(500).json({ error: 'Server error' });
-                                                                                                //   }
-                                                                                                // });
